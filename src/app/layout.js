0637081e3e52aa7b5e4f340419b686bb7e9bb54c
Matheus Alves <matheus.alves.stela@gmail.com>
2025-02-02 19:58:09 -0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex justify-center items-center">{children}</div>
       </body>
     </html>
   );
