bcfd3a89256a243ec06e9d67a25e8fc1df74aeaf
Matheus Alves <matheus.alves.stela@gmail.com>
2025-02-02 20:20:26 -0300

    segundo commit e ajustes gerais

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex justify-center items-center">{children}</div>
+        <Header/>
+        <div className="border m-1 flex justify-center items-center min-h-screen">{children}</div>
       </body>
     </html>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-center items-center h-[100px] border">
+      Header
+    </div>
+  );
+}
