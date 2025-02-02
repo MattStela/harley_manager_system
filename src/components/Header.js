148db6ad7676d0c3bb3f68946af9b6e6bbe0f86d
Matheus Alves <matheus.alves.stela@gmail.com>
2025-02-02 20:25:16 -0300

    adicionado logo - teste02

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <div className="flex flex-row justify-center items-center h-[100px] border">
-      Header
+      <Image src="/harley-white.png" width={100} height={100} alt="ícone de harley branca" />
     </div>
   );
 }
